27584db2ff60a8eb8af22ab04a36fe34b7c611ad
Alistair Grant <al.grant@bigpond.com>
2022-01-19 19:20:18 +1300

    Add calculator service

---
+++ src/WcfCalculator.Services/ICalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace WcfCalculator.Services
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
