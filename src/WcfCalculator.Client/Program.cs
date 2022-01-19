13f7b3e9a94f644d7b37e0521e607f4b5c7f1341
Alistair Grant <al.grant@bigpond.com>
2022-01-19 20:15:44 +1300

    Add console client

---
+++ src/WcfCalculator.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using WcfCalculator.Client.ServiceReferences;
+using static System.Console;
+
+namespace WcfCalculator.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                WriteLine("Press any key to invoke WCF service...");
+                ReadKey(true);
+
+                var random = new Random();
+                var n1 = random.Next(50);
+                var n2 = random.Next(50);
+
+                var result = AddWithCalculatorClient(n1, n2);
+
+                WriteLine($"{n1} + {n2} = {result}");
+            }
+            catch (Exception exception)
+            {
+                var originalColor = ForegroundColor;
+                ForegroundColor = ConsoleColor.Red;
+
+                Error.WriteLine(exception.Message);
+                Error.WriteLine(exception.StackTrace);
+
+                ForegroundColor = originalColor;
+            }
+
+            WriteLine();
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+
+        private static double AddWithCalculatorClient(double n1, double n2)
+        {
+            using (var client = new CalculatorClient())
+            {
+                return client.Add(n1, n2);
+            }
+        }
+    }
+}
